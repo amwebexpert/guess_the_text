1c298e11d4c76bb290d138459c80c7f303cda48f
Andre Masson <amwebexpert@gmail.com>
2022-03-20 21:23:19 -0600

    feat: preparing preferences screen skeleton

--- lib/screens/settings/settings_widget.dart
+++ lib/screens/settings/settings_widget.dart
@@ -1,24 +1,79 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:guess_the_text/theme/app_bar/app_bar_title_widget.dart';
+import 'package:guess_the_text/theme/theme_utils.dart';
+
+enum SingingCharacter { french, english }
 
 class SettingsWidget extends StatefulWidget {
   const SettingsWidget({Key? key}) : super(key: key);
 
   @override
-  _SettingsWidgetState createState() => _SettingsWidgetState();
+  State<SettingsWidget> createState() => _SettingsWidgetState();
 }
 
 class _SettingsWidgetState extends State<SettingsWidget> {
+  SingingCharacter? _character = SingingCharacter.french;
+
   @override
   Widget build(BuildContext context) {
     final AppLocalizations localizations = AppLocalizations.of(context)!;
 
     return Scaffold(
       appBar: AppBar(
-        title: AppBarTitle(title: localizations.preferences + '.'),
+        title: const AppBarTitle(title: 'Preferences'),
       ),
-      body: const Text('test'),
+      body: Padding(
+        padding: EdgeInsets.symmetric(vertical: spacing(1)),
+        child: Column(
+          children: <Widget>[
+            ListTile(
+              dense: true,
+              title: Text(
+                'Lafayette',
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+              onTap: () {
+                setState(() {
+                  _character = SingingCharacter.french;
+                });
+              },
+              leading: Radio<SingingCharacter>(
+                value: SingingCharacter.french,
+                activeColor: Theme.of(context).colorScheme.primary,
+                groupValue: _character,
+                onChanged: (SingingCharacter? value) {
+                  setState(() {
+                    _character = value;
+                  });
+                },
+              ),
+            ),
+            ListTile(
+              dense: true,
+              title: Text(
+                'Thomas Jefferson',
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+              onTap: () {
+                setState(() {
+                  _character = SingingCharacter.english;
+                });
+              },
+              leading: Radio<SingingCharacter>(
+                value: SingingCharacter.english,
+                activeColor: Theme.of(context).colorScheme.primary,
+                groupValue: _character,
+                onChanged: (SingingCharacter? value) {
+                  setState(() {
+                    _character = value;
+                  });
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
