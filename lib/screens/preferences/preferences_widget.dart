2cd073baead656b75aeac7802d9dd2b7e5d59866
Andre Masson <amwebexpert@gmail.com>
2022-03-20 21:09:37 -0600

    feat: preparing preferences screen skeleton

--- lib/screens/preferences/preferences_widget.dart
+++ lib/screens/preferences/preferences_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:guess_the_text/theme/app_bar/app_bar_title_widget.dart';
 
 class PreferencesWidget extends StatefulWidget {
   const PreferencesWidget({Key? key}) : super(key: key);
@@ -9,7 +10,12 @@ class PreferencesWidget extends StatefulWidget {
 
 class _PreferencesWidgetState extends State<PreferencesWidget> {
   @override
-  Widget build(BuildContext context) => const Center(
-        child: Text('test'),
-      );
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const AppBarTitle(title: 'Preferences'),
+      ),
+      body: const Text('test'),
+    );
+  }
 }
