b6a7146b80f024e1b9d02c36dc84d5866f34a4bc
Andre Masson <amwebexpert@gmail.com>
2022-04-16 14:35:13 -0500

    refactor: usage of immutability pattern for textToChange

--- lib/screens/game/text_to_guess_panel_widget.dart
+++ lib/screens/game/text_to_guess_panel_widget.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:guess_the_text/screens/game/text_to_guess_shuffling_widget.dart';
 import 'package:guess_the_text/screens/game/text_to_guess_with_category_widget.dart';
-import 'package:guess_the_text/store/game/game.store.dart';
 
 class TextToGuessArea extends StatelessWidget {
   final bool isShuffling;
@@ -10,14 +8,10 @@ class TextToGuessArea extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final GameStore gameStore = GameStore();
+    if (isShuffling) {
+      return const TextToGuessShuffling();
+    }
 
-    return Observer(builder: (BuildContext context) {
-      if (isShuffling) {
-        return const TextToGuessShuffling();
-      }
-
-      return TextToGuessWithCategory(textToGuess: gameStore.textToGuess);
-    });
+    return const TextToGuessWithCategory();
   }
 }
--- lib/screens/game/text_to_guess_with_category_widget.dart
+++ lib/screens/game/text_to_guess_with_category_widget.dart
@@ -1,32 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:guess_the_text/model/word_to_guess.dart';
 import 'package:guess_the_text/screens/game/text_to_guess_template_widget.dart';
 import 'package:guess_the_text/store/game/game.store.dart';
 
 class TextToGuessWithCategory extends StatelessWidget {
-  final TextToGuess textToGuess;
-
-  const TextToGuessWithCategory({Key? key, required this.textToGuess}) : super(key: key);
+  const TextToGuessWithCategory({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final GameStore gameStore = GameStore();
     final isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
 
-    return isPortrait
-        ? Column(
-            children: [
-              const CategoryLabel(),
-              TextToGuessTemplate(text: textToGuess.wordGame()),
-            ],
-          )
-        : Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              TextToGuessTemplate(text: textToGuess.wordGame()),
-              const CategoryLabel(),
-            ],
-          );
+    return Observer(builder: (BuildContext context) {
+      return isPortrait
+          ? Column(
+              children: [
+                const CategoryLabel(),
+                TextToGuessTemplate(text: gameStore.textToGuess.wordGame()),
+              ],
+            )
+          : Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                TextToGuessTemplate(text: gameStore.textToGuess.wordGame()),
+                const CategoryLabel(),
+              ],
+            );
+    });
   }
 }
 
