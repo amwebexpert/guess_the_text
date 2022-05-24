033e3cbbbc600758eb4da6ebbe639d1169e38ae6
Andre Masson <amwebexpert@gmail.com>
2022-05-24 13:33:47 -0400

    feat: preparing for store states

---
+++ lib/store/store.state.utils.dart
@@ -0,0 +1,18 @@
+import 'package:mobx/mobx.dart';
+import 'store.state.enum.dart';
+
+StoreState fromFuture(FutureStatus? status) {
+  switch (status) {
+    case FutureStatus.pending:
+      return StoreState.loading;
+
+    case FutureStatus.rejected:
+      return StoreState.error;
+
+    case FutureStatus.fulfilled:
+      return StoreState.success;
+
+    default:
+      return StoreState.initial;
+  }
+}
