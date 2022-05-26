21f5712886f1311abf960074df5b0f0beab4685f
Andre Masson <amwebexpert@gmail.com>
2022-05-26 15:37:54 -0400

    feat: preparing for store states

---
+++ test/mocks/api.text.mocks.dart
@@ -0,0 +1,15 @@
+import 'package:guess_the_text/features/game/api_text.model.dart';
+
+final mockTextToGuessFish = ApiText(
+  id: 1,
+  uuid: 'fish-uuid',
+  original: 'fish',
+  normalized: 'fish',
+);
+
+final mockTextToGuessWolf = ApiText(
+  id: 1,
+  uuid: 'wolf-uuid',
+  original: 'wolf',
+  normalized: 'wolf',
+);
