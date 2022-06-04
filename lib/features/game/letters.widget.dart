eb3bb6a88416effa59db0a78d007a200b39c72a8
Andre Masson <amwebexpert@gmail.com>
2022-06-04 08:30:36 -0400

    feat: animate the keyboard

--- lib/features/game/letters.widget.dart
+++ lib/features/game/letters.widget.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:guess_the_text/features/game/text_to_guess/text.to.guess.model.dart';
+import 'package:guess_the_text/service.locator.dart';
+import 'package:guess_the_text/store/fixed.delay.spinner.store.dart';
+import 'package:guess_the_text/store/store.state.enum.dart';
+import 'package:mobx/mobx.dart';
 
 import 'letter.widget.dart';
 
-class LettersWidget extends StatelessWidget {
+class LettersWidget extends StatefulWidget {
   static const List<String> letters = [
     'A',
     'B',
@@ -39,14 +43,67 @@ class LettersWidget extends StatelessWidget {
   const LettersWidget({Key? key, required this.textToGuess, required this.onLetterPressed}) : super(key: key);
 
   @override
+  State<LettersWidget> createState() => _LettersWidgetState();
+}
+
+class _LettersWidgetState extends State<LettersWidget> with SingleTickerProviderStateMixin {
+  final FixedDelaySpinnerStore spinnerStore = serviceLocator.get();
+  late final AnimationController animController;
+  late final Animation<double> rotationAnim;
+
+  final List<ReactionDisposer> disposers = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    // prepare animation
+    animController = AnimationController(duration: const Duration(milliseconds: 1200), vsync: this);
+    animController.addListener(() {
+      setState(() {});
+    });
+
+    rotationAnim = Tween<double>(begin: 0.0, end: 1.0).chain(CurveTween(curve: Curves.ease)).animate(animController);
+
+    // link animation to spinnerStore state change
+    final ReactionDisposer disposer = reaction((_) => spinnerStore.state, (StoreState storeState) {
+      if (storeState == StoreState.loading) {
+        animController.reverse();
+      }
+    });
+    disposers.add(disposer);
+  }
+
+  @override
+  void dispose() {
+    animController.dispose();
+
+    for (var disposer in disposers) {
+      disposer();
+    }
+
+    super.dispose();
+  }
+
+  void onPressed(String c) {
+    animController.forward(from: 0);
+    widget.onLetterPressed(c);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Wrap(
       direction: Axis.horizontal,
       alignment: WrapAlignment.center,
       spacing: 1,
-      children: letters
-          .map((c) =>
-              LetterWidget(letter: c, isButtonEnable: !textToGuess.isCharTried(c), onLetterPressed: onLetterPressed))
+      children: LettersWidget.letters
+          .map(
+            (c) => RotationTransition(
+                turns: rotationAnim,
+                alignment: Alignment.centerLeft,
+                child: LetterWidget(
+                    letter: c, isButtonEnable: !widget.textToGuess.isCharTried(c), onLetterPressed: onPressed)),
+          )
           .toList(),
     );
   }
