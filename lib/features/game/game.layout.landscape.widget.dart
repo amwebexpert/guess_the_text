7bb97ebb6b9e127cec5ec63fb0a8cc7403845f5f
Andre Masson <amwebexpert@gmail.com>
2022-06-17 09:26:27 -0400

    feat: responsive layouts

--- lib/features/game/game.layout.landscape.widget.dart
+++ lib/features/game/game.layout.landscape.widget.dart
@@ -2,16 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:guess_the_text/features/game/game.image.widget.dart';
 import 'package:guess_the_text/features/game/game.interaction.panel.widget.dart';
 import 'package:guess_the_text/features/game/text.and.category.widget.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 class GameLayoutLandscapeWidget extends StatelessWidget {
   const GameLayoutLandscapeWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final leftFlexWeight = ResponsiveValue(
+      context,
+      defaultValue: 1,
+      valueWhen: const [Condition.largerThan(name: TABLET, value: 1)],
+    ).value!;
+    final rightFlexWeight = ResponsiveValue(
+      context,
+      defaultValue: 1,
+      valueWhen: const [Condition.largerThan(name: TABLET, value: 2)],
+    ).value!;
+
     return Row(
-      children: const <Widget>[
-        Flexible(flex: 1, child: TextAndKeyboardWidget()),
-        Flexible(flex: 1, child: GameImageWidget()),
+      children: <Widget>[
+        Flexible(flex: leftFlexWeight, child: const TextAndKeyboardWidget()),
+        Flexible(flex: rightFlexWeight, child: const GameImageWidget()),
       ],
     );
   }
