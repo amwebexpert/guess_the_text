add0f24ed7b663810f79df04dada460a754fd188
Andre Masson <amwebexpert@gmail.com>
2022-06-21 08:09:41 -0400

    feat: colors gradient for the menu header bulb

--- lib/theme/widgets/menu.logo.widget.dart
+++ lib/theme/widgets/menu.logo.widget.dart
@@ -6,10 +6,16 @@ class MenuLogo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final bool isDark = Theme.of(context).brightness == Brightness.dark;
+    final LinearGradient gradient = isDark
+        ? const LinearGradient(
+            colors: [Colors.white, Colors.black], begin: Alignment.topCenter, end: Alignment.bottomCenter)
+        : const LinearGradient(
+            colors: [Colors.orange, Colors.white], begin: Alignment.topCenter, end: Alignment.bottomCenter);
     const assetName = 'assets/images/drawer-header.png';
 
     return DrawerHeader(
       decoration: BoxDecoration(
+          gradient: gradient,
           image: DecorationImage(invertColors: isDark, fit: BoxFit.contain, image: const AssetImage(assetName))),
       child: const Text(''),
     );
