d434939bf7ee937b7c21993df2c6a411a9374089
Andre Masson <amwebexpert@gmail.com>
2022-06-27 06:35:52 -0400

    style: prettify the menu logo

--- lib/theme/widgets/menu.logo.widget.dart
+++ lib/theme/widgets/menu.logo.widget.dart
@@ -1,25 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:guess_the_text/theme/theme.utils.dart';
 
 class MenuLogo extends StatelessWidget {
   const MenuLogo({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final AppLocalizations localizations = AppLocalizations.of(context)!;
     final bool isDark = Theme.of(context).brightness == Brightness.dark;
-    final LinearGradient gradient = isDark
-        ? const LinearGradient(
-            colors: [Colors.grey, Colors.black], begin: Alignment.topCenter, end: Alignment.bottomCenter)
-        : LinearGradient(
-            colors: [Theme.of(context).colorScheme.primary, Colors.white],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter);
+    final Color color = isDark ? Colors.white : Colors.black;
     const assetName = 'assets/images/drawer-header.png';
 
     return DrawerHeader(
-      decoration: BoxDecoration(
-          gradient: gradient,
-          image: DecorationImage(invertColors: isDark, fit: BoxFit.contain, image: const AssetImage(assetName))),
-      child: const Text(''),
+      child: Center(
+        child: Column(
+          children: [
+            Expanded(
+              child: Image.asset(
+                assetName,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+            ),
+            SizedBox(height: spacing(1)),
+            Text(
+              localizations.appTitle,
+              style: Theme.of(context).textTheme.bodyText2,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
