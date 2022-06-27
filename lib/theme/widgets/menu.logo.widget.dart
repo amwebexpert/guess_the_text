62132be0c5818301fe6c86451f5b792a1530d42f
Andre Masson <amwebexpert@gmail.com>
2022-06-27 06:39:54 -0400

    style: prettify the menu logo

--- lib/theme/widgets/menu.logo.widget.dart
+++ lib/theme/widgets/menu.logo.widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:guess_the_text/theme/theme.utils.dart';
 
 class MenuLogo extends StatelessWidget {
@@ -8,14 +9,14 @@ class MenuLogo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final AppLocalizations localizations = AppLocalizations.of(context)!;
-    const assetName = 'assets/images/drawer-header.png';
+    const assetName = 'assets/images/drawer-header.svg';
 
     return DrawerHeader(
       child: Center(
         child: Column(
           children: [
             Expanded(
-              child: Image.asset(
+              child: SvgPicture.asset(
                 assetName,
                 color: Theme.of(context).colorScheme.primary,
               ),
