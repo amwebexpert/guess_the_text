5248079e47a042c84c247c90e4d17d0be0f48739
André Masson <amwebexpert@gmail.com>
2022-07-09 06:47:34 -0400

    feat: add connectivity service usage example (#64)
    * feat: add connectivity service usage example
    
    * feat: add connectivity service usage example

---
+++ lib/features/about/card.app.connectivity.status.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+
+class ConnectivityStatusWidget extends StatefulWidget {
+  const ConnectivityStatusWidget({Key? key}) : super(key: key);
+
+  @override
+  State<ConnectivityStatusWidget> createState() => _ConnectivityStatusWidgetState();
+}
+
+class _ConnectivityStatusWidgetState extends State<ConnectivityStatusWidget> {
+  late StreamSubscription<ConnectivityResult> _subscription;
+  ConnectivityResult? _connectivityResult;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _subscription = Connectivity().onConnectivityChanged.listen((ConnectivityResult result) {
+      setState(() {
+        _connectivityResult = result;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _subscription.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final stasusText = _connectivityResult?.toString().split('.')[1] ?? '-';
+    return Text(stasusText);
+  }
+}
