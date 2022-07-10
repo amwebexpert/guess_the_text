67bc3a0e8fe1e3fa8b2203e380ca536c88f040ee
André Masson <amwebexpert@gmail.com>
2022-07-10 08:09:31 -0400

    fix: landscape game layout extra right padding (#69)
    * fix: remove extra landscape right side margin
    
    * fix: remove extra landscape right side margin
    
    * fix: remove extra landscape right side margin
    
    * fix: remove extra landscape right side margin

--- lib/features/game/game.layout.landscape.widget.dart
+++ lib/features/game/game.layout.landscape.widget.dart
@@ -4,28 +4,29 @@ import 'package:responsive_framework/responsive_framework.dart';
 import '/features/game/game.image.widget.dart';
 import '/features/game/game.interaction.panel.widget.dart';
 import '/features/game/text.and.category.widget.dart';
+import '/theme/theme.utils.dart';
 
 class GameLayoutLandscapeWidget extends StatelessWidget {
   const GameLayoutLandscapeWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final leftFlexWeight = ResponsiveValue(
-      context,
-      defaultValue: 1,
-      valueWhen: const [Condition.largerThan(name: TABLET, value: 1)],
-    ).value!;
+    const leftFlexWeight = 1;
     final rightFlexWeight = ResponsiveValue(
       context,
       defaultValue: 1,
       valueWhen: const [Condition.largerThan(name: TABLET, value: 2)],
     ).value!;
 
-    return Row(
-      children: <Widget>[
-        Flexible(flex: leftFlexWeight, child: const TextAndKeyboardWidget()),
-        Flexible(flex: rightFlexWeight, child: const GameImageWidget()),
-      ],
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: spacing(1)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Flexible(flex: leftFlexWeight, fit: FlexFit.tight, child: TextAndKeyboardWidget()),
+          Flexible(flex: rightFlexWeight, fit: FlexFit.tight, child: const GameImageWidget()),
+        ],
+      ),
     );
   }
 }
