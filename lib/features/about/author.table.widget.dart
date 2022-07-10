c98d6e9702cff5507a0ddc6b5dee45d36bc9901d
jefvda <jef.v.d.a@live.be>
2022-07-10 23:04:30 +0200

    feat: add table for multiple authors
    Added layout for table with multiple authors

--- lib/features/about/author.table.widget.dart
+++ lib/features/about/author.table.widget.dart
@@ -1,4 +1,8 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:guess_the_text/features/about/contributor.model.dart';
+
+import '../../theme/widgets/text.link.widget.dart';
 
 class AuthorTable extends StatefulWidget {
   const AuthorTable({Key? key}) : super(key: key);
@@ -8,8 +12,42 @@ class AuthorTable extends StatefulWidget {
 }
 
 class _AuthorTableState extends State<AuthorTable> {
+  final List<Contributor> _contributors = <Contributor>[
+    Contributor(name:  'André Masson', email: 'amwebexpert@gmail.com', linkedIn: 'https://www.linkedin.com/in/amwebexpert'),
+    Contributor(name: 'Jef van der Avoirt', email: 'jef.v.d.a@live.be', linkedIn: 'https://www.linkedin.com/in/jefvda'),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final AppLocalizations localizations = AppLocalizations.of(context)!;
+
+    return Column(
+      children: [
+        DataTable(
+          horizontalMargin: 0,
+          columnSpacing: 16,
+          headingRowHeight: 20,
+          dataRowHeight: 24,
+          columns: <DataColumn>[
+            DataColumn(
+              label: Text(localizations.appAuthorName),
+            ),
+            DataColumn(
+              label: Text(localizations.appAuthorProfile),
+            ),
+          ],
+          rows: _contributors.map((contributor) => DataRow(
+              cells: <DataCell> [
+                DataCell(
+                  ThemedTextLink(displayText: contributor.name, hyperlink: 'mailto:${contributor.email}'),
+                ),
+                DataCell(
+                  ThemedTextLink(displayText: 'Linked-in', hyperlink: contributor.linkedIn),
+                )
+              ]
+          )).toList(),
+        ),
+      ],
+    );
   }
 }
