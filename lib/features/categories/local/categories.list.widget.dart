b5d2682033c6b33319556c75ae89f4bae9b29e06
André Masson <andre.masson@lacapitale.com>
2022-08-13 12:03:54 -0400

    feat: protection for category removal

--- lib/features/categories/local/categories.list.widget.dart
+++ lib/features/categories/local/categories.list.widget.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:guess_the_text/features/categories/category.icons.map.dart';
 import 'package:guess_the_text/features/categories/local/edit.category.widget.dart';
 import 'package:guess_the_text/service.locator.dart';
 import 'package:guess_the_text/services/text.service/api.category.model.dart';
 import 'package:guess_the_text/services/text.service/sql.db.service.dart';
+import 'package:guess_the_text/theme/widgets/snackbar/snackbar.model.dart';
+import 'package:guess_the_text/theme/widgets/snackbar/snackbar.utils.dart';
 import 'package:guess_the_text/utils/language.utils.dart';
 
 import '/theme/theme.utils.dart';
@@ -48,40 +51,57 @@ class _CategoriesListWidgetState extends State<CategoriesListWidget> {
     }
   }
 
-  Future<void> _deleteCategory(ApiCategory category) async {
+  Future<void> _deleteCategory(BuildContext context, ApiCategory category, int index) async {
     await sqlDbService.deleteCategory(category);
     setState(() => categories.remove(category));
+
+    if (!mounted) {
+      return;
+    }
+
+    final AppLocalizations localizations = AppLocalizations.of(context)!;
+    final action = SnackBarAction(
+        label: 'UNDO',
+        onPressed: () {
+          sqlDbService.createCategory(category).then((restoredCategory) {
+            setState(() => categories.insert(index, restoredCategory));
+          });
+        });
+    showAppSnackbar(
+        context: context,
+        message: localizations.categoryDeletedMessage(category.name),
+        type: SnackbarType.info,
+        milliseconds: 4 * 1000,
+        action: action);
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        floatingActionButton:
-            FloatingActionButton(child: const Icon(Icons.add), onPressed: () => _createCategory(context)),
-        body: FullScreenAssetBackground(
-          assetImagePath: CategoriesListWidget.backgroundImage,
-          child: Padding(
-            padding: EdgeInsets.all(spacing(2)),
-            child: ListView.builder(
-                itemCount: categories.length,
-                itemBuilder: (context, index) {
-                  final category = categories[index];
-                  final id = category.id.toString();
+  Widget build(BuildContext context) => Scaffold(
+      floatingActionButton:
+          FloatingActionButton(child: const Icon(Icons.add), onPressed: () => _createCategory(context)),
+      body: FullScreenAssetBackground(
+        assetImagePath: CategoriesListWidget.backgroundImage,
+        child: Padding(
+          padding: EdgeInsets.all(spacing(2)),
+          child: ListView.builder(
+              itemCount: categories.length,
+              itemBuilder: (context, index) {
+                final category = categories[index];
+                final id = category.id.toString();
 
-                  return Dismissible(
-                      key: Key(id),
-                      onDismissed: (direction) => _deleteCategory(category),
-                      child: Card(
-                        key: ValueKey(id),
-                        child: ListTile(
-                          leading: Icon(categoryIcons[category.iconName]),
-                          title: Text(category.name),
-                          subtitle: Text(getLanguageFullNameFromCode(context, category.langCode)),
-                          onTap: () => _updateCategory(context, category, index),
-                        ),
-                      ));
-                }),
-          ),
-        ));
-  }
+                return Dismissible(
+                    key: Key(id),
+                    onDismissed: (direction) => _deleteCategory(context, category, index),
+                    child: Card(
+                      key: ValueKey(id),
+                      child: ListTile(
+                        leading: Icon(categoryIcons[category.iconName]),
+                        title: Text(category.name),
+                        subtitle: Text(getLanguageFullNameFromCode(context, category.langCode)),
+                        onTap: () => _updateCategory(context, category, index),
+                      ),
+                    ));
+              }),
+        ),
+      ));
 }
