25982476d933a6d6f29ff63926b87cdc427642a7
André Masson <andre.masson@lacapitale.com>
2022-08-13 10:57:35 -0400

    feat: persist text items with associated category

--- lib/services/text.service/sql.db.service.dart
+++ lib/services/text.service/sql.db.service.dart
@@ -5,12 +5,35 @@ import 'package:guess_the_text/services/file/directory.enum.dart';
 import 'package:guess_the_text/services/file/file.service.dart';
 import 'package:guess_the_text/services/logger/logger.service.dart';
 import 'package:guess_the_text/services/text.service/api.category.model.dart';
+import 'package:guess_the_text/services/text.service/api.text.model.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 enum TableNames { category, text }
 
 enum CategoryColumns { id, uuid, langcode, name, iconname }
+
+enum TextColumns { id, categoryid, uuid, original, normalized }
+
+final _createTableCategory = '''
+  CREATE TABLE ${TableNames.category.name} (
+    ${CategoryColumns.id.name} INTEGER PRIMARY KEY,
+    ${CategoryColumns.uuid.name} TEXT,
+    ${CategoryColumns.langcode.name} TEXT,
+    ${CategoryColumns.name.name} TEXT,
+    ${CategoryColumns.iconname.name} TEXT
+  )
+''';
+
+final _createTableText = '''
+  CREATE TABLE ${TableNames.text.name} (
+    ${TextColumns.id.name} INTEGER PRIMARY KEY,
+    ${TextColumns.categoryid.name} INTEGER,
+    ${TextColumns.uuid.name} TEXT,
+    ${TextColumns.original.name} TEXT,
+    ${TextColumns.normalized.name} TEXT
+  )
+''';
 
 class SqlDbService {
   final LoggerService logger = serviceLocator.get();
@@ -19,16 +42,6 @@ class SqlDbService {
   static final SqlDbService _instance = SqlDbService._privateConstructor();
   Database? _db;
   final version = 1;
-
-  final _createTableCategory = '''
-    CREATE TABLE ${TableNames.category.name} (
-      ${CategoryColumns.id.name} INTEGER PRIMARY KEY,
-      ${CategoryColumns.uuid.name} TEXT,
-      ${CategoryColumns.langcode.name} TEXT,
-      ${CategoryColumns.name.name} TEXT,
-      ${CategoryColumns.iconname.name} TEXT
-    )
-  ''';
 
   factory SqlDbService() => _instance;
   SqlDbService._privateConstructor();
@@ -58,6 +71,7 @@ class SqlDbService {
   Future<void> _onCreate(Database db, int version) async {
     logger.info('creating sqflite db version $version');
     await db.execute(_createTableCategory);
+    await db.execute(_createTableText);
   }
 
   Future<List<ApiCategory>> getCategories() async {
@@ -88,5 +102,26 @@ class SqlDbService {
     final where = '${CategoryColumns.id.name} = ?';
     int result = await _getDb().delete(TableNames.category.name, where: where, whereArgs: [category.id]);
     logger.info('removed category ${category.name}, result: $result');
+  }
+
+  Future<ApiText> createText(ApiText text) async {
+    Map<String, dynamic> toInsert = text.toJson()..remove(TextColumns.id.name); // the 'id' value will be auto-generated
+    int newId = await _getDb().insert(TableNames.text.name, toInsert);
+    logger.info('created text id $newId - ${text.original}');
+    return text.copyWith(id: newId);
+  }
+
+  Future<ApiText> updateText(ApiText text) async {
+    Map<String, dynamic> toUpdate = text.toJson();
+    final where = '${TextColumns.id.name} = ?';
+    int result = await _getDb().update(TableNames.text.name, toUpdate, where: where, whereArgs: [text.id]);
+    logger.info('updated text ${text.original}, result: $result');
+    return text;
+  }
+
+  Future<void> deleteText(ApiText text) async {
+    final where = '${TextColumns.id.name} = ?';
+    int result = await _getDb().delete(TableNames.text.name, where: where, whereArgs: [text.id]);
+    logger.info('removed text ${text.original}, result: $result');
   }
 }
