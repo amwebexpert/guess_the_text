67b2034ba4a869c9e2f9b40877ca6d328dac743d
André Masson <andre.masson@lacapitale.com>
2022-08-28 12:58:22 -0400

    test: added fold method experiments

--- test/dart.experiments/dart.fold_test.dart
+++ test/dart.experiments/dart.fold_test.dart
@@ -9,37 +9,39 @@ class WordStats {
 
 void main() {
   group('Dart fold() method', () {
+    List<String> words = ['Hello', 'Up', 'Up', 'Up', 'World', 'World', 'Door', 'Down', 'Handle', 'Down', 'Up', 'Down'];
+
     test('should reduce with collector', () {
       // given
-      const List<WordStats> stats = [];
-      const Map<String, int> counts = {};
-      List<String> words = [
-        'Hello',
-        'Up',
-        'Up',
-        'Up',
-        'World',
-        'World',
-        'Door',
-        'Down',
-        'Handle',
-        'Down',
-        'Up',
-        'Down'
-      ];
 
       // when
-      final Map<String, int> allStats = words.fold({}, (acc, word) {
+      final Map<String, int> collectedStats = words.fold({}, (acc, word) {
         acc[word] = (acc[word] != null) ? acc[word]! + 1 : 1;
         return acc;
       });
 
       // then
       // expecting smtg like: {Hello: 1, Up: 4, World: 2, Door: 1, Down: 3, Handle: 1}
-      expect(allStats, isNotNull);
-      expect(allStats.length, 6);
-      expect(allStats['Hello'], 1);
-      expect(allStats['Up'], 4);
+      expect(collectedStats, isNotNull);
+      expect(collectedStats.length, 6);
+      expect(collectedStats['Hello'], 1);
+      expect(collectedStats['Up'], 4);
+    });
+
+    test('should reduce with collector and transform to List', () {
+      // given
+      final Map<String, int> collectedStats = words.fold({}, (acc, word) {
+        acc[word] = (acc[word] != null) ? acc[word]! + 1 : 1;
+        return acc;
+      });
+
+      // when
+      final List<WordStats> stats =
+          collectedStats.entries.map((entry) => WordStats(word: entry.key, count: entry.value)).toList();
+
+      // then
+      expect(stats, isNotNull);
+      expect(stats.length, 6);
     });
   });
 }
