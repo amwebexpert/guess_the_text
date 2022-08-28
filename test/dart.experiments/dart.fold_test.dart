214200c784b409c58c87958f5a7b07fa41d3a502
André Masson <andre.masson@lacapitale.com>
2022-08-28 13:27:59 -0400

    test: added fold method experiments

--- test/dart.experiments/dart.fold_test.dart
+++ test/dart.experiments/dart.fold_test.dart
@@ -1,10 +1,16 @@
 import 'package:flutter_test/flutter_test.dart';
 
-class WordStats {
+class WordStats implements Comparable {
   String word;
   int count;
 
   WordStats({required this.word, required this.count});
+
+  @override
+  String toString() => '$word : $count';
+
+  @override
+  int compareTo(other) => word.compareTo(other.word);
 }
 
 void main() {
@@ -42,6 +48,27 @@ void main() {
       // then
       expect(stats, isNotNull);
       expect(stats.length, 6);
+    });
+
+    test('should reduce with collector and transform to ordered List', () {
+      // given
+      final Map<String, int> collectedStats = words.fold({}, (acc, word) {
+        acc[word] = (acc[word] != null) ? acc[word]! + 1 : 1;
+        return acc;
+      });
+
+      // when
+      final List<WordStats> stats =
+          collectedStats.entries.map((entry) => WordStats(word: entry.key, count: entry.value)).toList()
+            //..sort(); // using Comparable impl of the elements
+            ..sort((w1, w2) => w2.count.compareTo(w1.count));
+
+      // then
+      print(stats);
+      expect(stats, isNotNull);
+      expect(stats.length, 6);
+      expect(stats.first.word, 'Up');
+      expect(stats.first.count, 4);
     });
   });
 }
